7e49000e92c419933c03fb0af1a31417fb1ada05
indo.office3 <indo.office3@kasumi.co.jp>
2020-07-08 10:42:07 +0700

    buildone

---
+++ src/contexts/ApiContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ApiContext = () => {
+    return (
+        <View>
+            <Text>ewewe</Text>
+        </View>
+    )
+}
+
+export default ApiContext
---
+++ src/reducers/ApiReducers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ApiReducers = () => {
+    return (
+        <View>
+            <Text>qwqw</Text>
+        </View>
+    )
+}
+
+export default ApiReducers
